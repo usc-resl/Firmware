5c284faf6d5c82be18ef508f39b5e897a938f3f6
Joerg Mueller <joerg.mueller@usc.edu>
2014-06-13 17:37:33 -0700

    * fixed arming for offboard position and velocity control

--- src/modules/mavlink/mavlink_receiver.cpp
+++ src/modules/mavlink/mavlink_receiver.cpp
@@ -924,12 +924,14 @@ MavlinkReceiver::handle_message_offboard_control (mavlink_message_t *msg)
 
 		case 1:
 			ml_mode = OFFBOARD_CONTROL_MODE_DIRECT_RATES;
-			ml_armed = true;
+			if (offboard_control_msg.p4 > 0)
+				ml_armed = true;
 			break;
 
 		case 2:
 			ml_mode = OFFBOARD_CONTROL_MODE_DIRECT_ATTITUDE;
-			ml_armed = true;
+			if (offboard_control_msg.p4 > 0)
+				ml_armed = true;
 			break;
 
 		case 3:
@@ -949,10 +951,6 @@ MavlinkReceiver::handle_message_offboard_control (mavlink_message_t *msg)
   offboard_control_sp.p2 = (float)offboard_control_msg.p2;
   offboard_control_sp.p3 = (float)offboard_control_msg.p3;
   offboard_control_sp.p4 = (float)offboard_control_msg.p4;
-
-  if (offboard_control_msg.p4 == 0) {
-    ml_armed = false;
-  }
 
   offboard_control_sp.armed = ml_armed;
   offboard_control_sp.mode = ml_mode;
